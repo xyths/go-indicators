3f2381aab567fd4de71c9ee147c9b347386b0203
Alex <alex@xingyongtao.xyz>
2020-10-15 10:38:05 +0800

    upgrade squeeze indicator

--- squeezemomentum.go
+++ squeezemomentum.go
@@ -2,13 +2,37 @@ package indicator
 
 import "github.com/markcheno/go-talib"
 
+const (
+	Black int = 1
+	Gray      = -1
+	Blue      = 0
+)
+
+type Result struct {
+	Trend int // 1 squeeze, 2 up, -2 down, 0 stop
+	Last  int
+}
+
+type Detail struct {
+	Value      []float64
+	SqueezeOn  []bool
+	SqueezeOff []bool
+	NoSqueeze  []bool
+	KCUpper    []float64
+	KCMiddle   []float64
+	KCLower    []float64
+	BBUpper    []float64
+	BBMiddle   []float64
+	BBLower    []float64
+}
+
 // Squeeze Momentum Indicator [LazyBear]
 // https://cn.tradingview.com/script/nqQ1DT5a-Squeeze-Momentum-Indicator-LazyBear/
 // bbl: BB Length
 // kcl: KC Length
 // bbf: BB MultFactor
 // kcf: KC MulFactor
-func SqueezeMomentum(bbl, kcl int, bbf, kcf float64, inHigh, inLow, inClose []float64) ([]float64, []bool, []bool, []bool) {
+func Squeeze(bbl, kcl int, bbf, kcf float64, inHigh, inLow, inClose []float64) (r Result, d Detail) {
 	basis := talib.Sma(inClose, bbl)
 	dev := talib.StdDev(inClose, bbl, bbf)
 	upperBB := talib.Add(basis, dev)
@@ -41,14 +65,63 @@ func SqueezeMomentum(bbl, kcl int, bbf, kcf float64, inHigh, inLow, inClose []fl
 	avg2 := talib.MedPrice(avg1, ma)
 	target := talib.Sub(inClose, avg2)
 	val := talib.LinearReg(target, kcl)
-	return val, squeezeOn, squeezeOff, noSqueeze
+	d.Value = val
+	d.SqueezeOn = squeezeOn
+	d.SqueezeOff = squeezeOff
+	d.NoSqueeze = noSqueeze
+	d.KCUpper = upperKC
+	d.KCMiddle = ma
+	d.KCLower = lowerKC
+	r = Summary(d)
+	return
 }
-//
-//func Squeeze(bbl, kcl int, bbf, kcf float64, inHigh, inLow, inClose []float64) ([]float64, []bool, []bool, []bool) {
-//	val, squeezeOn, squeezeOff, noSqueeze := SqueezeMomentum(bbl, kcl, bbf, kcf, inHigh, inLow, inClose)
-//
-//}
-//
-//func SqueezeTrend(val []float64, squeezeOn, squeezeOff, noSqueeze []bool, []bool, []bool) (compression int, ) {
-//
-//}
+
+func Summary(d Detail) (r Result) {
+	l := len(d.Value)
+	current := l - 2
+	if l < 3 {
+		return
+	}
+	if d.NoSqueeze[current] {
+		return
+	}
+
+	if d.SqueezeOn[current] {
+		// count the dark cross
+		last := 0
+		for i := current; d.SqueezeOn[i] && i >= 0; i-- {
+			last++
+		}
+		r.Trend = 1 // squeeze
+		r.Last = last
+
+	} else {
+		// find first gray cross
+		firstGrayIndex := 0
+		for i := current; !d.SqueezeOn[i] && i >= 0; i-- {
+			firstGrayIndex = i
+		}
+		isUptrend := d.Value[firstGrayIndex] > 0
+		trendStopped := false
+		stopIndex := 0
+		for i := firstGrayIndex; i <= current; i++ {
+			if isUptrend && d.Value[i] <= d.Value[i-1] || !isUptrend && d.Value[i] >= d.Value[i-1] {
+				trendStopped = true
+				stopIndex = i
+				break
+			}
+		}
+		if !trendStopped {
+			if isUptrend {
+				r.Trend = 2
+			} else {
+				r.Trend = -2
+			}
+			r.Last = current - firstGrayIndex + 1
+		} else {
+			r.Trend = 0
+			r.Last = current - stopIndex + 1
+		}
+	}
+	return
+}
