54972d444554c1b10b43911b0d7988a88d8fc69a
Alex <alex@xingyongtao.xyz>
2020-10-15 14:10:04 +0800

    add keltner channel util functions

--- ta.go
+++ ta.go
@@ -16,3 +16,24 @@ func Keltner(factor float64, mean, atr []float64) (upper, lower []float64) {
 	lower = talib.Sub(mean, atr2)
 	return
 }
+
+// Keltner Channel used by LazyBear
+func KCLB(inHigh, inLow, inClose []float64, inTimePeriod int, multiplier float64) (upper, basis, lower []float64) {
+	return KC(inHigh, inLow, inClose, inTimePeriod, multiplier, talib.SMA, talib.SMA)
+}
+
+// Keltner Channel used by TradingView, use EMA as default
+func KCTV(inHigh, inLow, inClose []float64, inTimePeriod int, multiplier float64) (upper, basis, lower []float64) {
+	// TODO: TradingView use RMA here
+	return KC(inHigh, inLow, inClose, inTimePeriod, multiplier, talib.EMA, talib.EMA)
+}
+
+func KC(inHigh, inLow, inClose []float64, inTimePeriod int, multiplier float64, basisMAType, rangeMAType talib.MaType) (upper, basis, lower []float64) {
+	basis = talib.Ma(inClose, inTimePeriod, basisMAType)
+	tr := talib.TRange(inHigh, inLow, inClose)
+	range_ := talib.Ma(tr, inTimePeriod, rangeMAType)
+	band := Multiply(range_, multiplier)
+	upper = talib.Add(basis, band)
+	lower = talib.Sub(basis, band)
+	return
+}
